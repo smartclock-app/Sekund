ca128f5c833c8c387372e12aaac576ef7b728231
Danny Peak <68485078+0x5045414b@users.noreply.github.com>
2026-02-08 22:25:27 +0000

    Hide android status bar

--- src-tauri/gen/android/app/src/main/java/uk/co/danpeak/smartclock/clock/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/uk/co/danpeak/smartclock/clock/MainActivity.kt
@@ -2,10 +2,19 @@ package uk.co.danpeak.smartclock.clock
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
+
+    WindowCompat.getInsetsController(window, window.decorView).apply {
+        hide(WindowInsetsCompat.Type.systemBars())
+        systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+
     super.onCreate(savedInstanceState)
   }
 }
